e260eda896c3858efef71442e178e23aaa5420ef
Sofia Chaikovs'ka <sofiya0511@gmail.com>
2024-11-16 23:28:59 +0200

    Added info about me

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,9 +4,9 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/sofia.png"} alt="Sofia Chaikovska" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
+          <h2 style={styles.name}>Sofia Chaikovska</h2>
           <p style={styles.position}>Software engineer</p>
         </div>
       </div>
