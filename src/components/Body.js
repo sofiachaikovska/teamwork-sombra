9993f0e31bfe5c9cd6b4ca22f54a1fb5c1650e60
roksolana-shendiukh <roksolana.shendiukh@gmail.com>
2024-11-16 23:03:12 +0200

    wewe

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,9 +4,9 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"roksolana.jpg"} alt="Roksolana" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
+          <h2 style={styles.name}>Roksolana Shendiukh</h2>
           <p style={styles.position}>Software engineer</p>
         </div>
       </div>
