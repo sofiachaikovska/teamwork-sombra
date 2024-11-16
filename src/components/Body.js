e8014667606205ab5a8c9b17ad69c9836cd30380
Myroslav13 <myroslavshchuruk@gmail.com>
2024-11-16 23:02:53 +0200

    Add info about me

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/myroslav-img.png"} alt="Shchuruk Myroslav" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Shchuruk Myroslav</h2>
+          <p style={styles.position}>Tester</p>
         </div>
       </div>
     </main>
